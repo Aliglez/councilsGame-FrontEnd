a63ff5a555435943f45447798d1ff9dfac8c5dab
Alicia González Castro <aliglezcastro@gmail.com>
2024-03-21 18:20:06 +0100

    banner in homepage with text and styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    
+    <div class="card bg-dark text-white">
+      <img
+        class="card-img"
+        src="../assets/img/bannerUrriellu.jpg"
+        alt="Panorámica de Urriellu"
+      />
+      <div class="card-img-overlay">
+        <p class="card-text">
+          Asturias, una región que parece haber sido diseñada para el placer.
+        </p>
+      </div>
+    </div>
   </main>
 </template>
+<style scoped lang="scss">
+@import "../assets/variables";
+
+.card  {
+  border: none;
+}
+
+.card-img-overlay {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: flex-end;
+  
+}
+
+.card-text {
+  font-family: $first-font;
+  font-size: 3rem;
+  text-align: right;
+  color: white;
+  width: 50%;
+  display: right;
+
+  @media screen and (max-width: 600px) {
+    font-size: 1rem;
+  }
+}
+</style>
